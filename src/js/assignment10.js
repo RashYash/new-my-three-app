846daa3bb554426abe6d04e5fe01ee8058db9183
Yasara <ryasara2000@gmail.com>
2026-05-27 08:56:47 +0530

    Updated  assignment 10 files

--- src/js/assignment10.js
+++ src/js/assignment10.js
@@ -10,11 +10,11 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.set(0, 3, 8);
+camera.position.set(4, 3, 8);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("model-container").appendChild(renderer.domElement);
+document.getElementById("model-container10").appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -23,14 +23,24 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
-
 directionalLight.position.set(5, 10, 5);
 scene.add(directionalLight);
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load(import.meta.env.BASE_URL + "texture2.jpg");
+
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(15, 15),
+  new THREE.MeshStandardMaterial({ map: texture }),
+);
+floor.rotation.x = -Math.PI / 2;
+scene.add(floor);
 
 const loader = new GLTFLoader();
 
 let car;
 let carBody;
+let carRim;
 
 loader.load(
   import.meta.env.BASE_URL + "assets/model/car.glb",
@@ -38,7 +48,7 @@ loader.load(
   function (gltf) {
     car = gltf.scene;
 
-    car.scale.set(0.5, 0.5, 0.5);
+    car.scale.set(0.7, 0.7, 0.7);
     scene.add(car);
 
     car.traverse((child) => {
@@ -46,20 +56,26 @@ loader.load(
     });
 
     carBody = car.getObjectByName("Object_32");
+    carRim = car.getObjectByName("Object_23");
   },
 );
+function setColor(buttonId, getObject, color) {
+  document.getElementById(buttonId).onclick = () => {
+    const object = getObject();
 
-document.getElementById("goldCar").onclick = () => {
-  if (carBody) {
-    carBody.material.color.set(0xb8860b);
-  }
-};
-
-document.getElementById("blueCar").onclick = () => {
-  if (carBody) {
-    carBody.material.color.set(0x0000ff);
-  }
-};
+    if (object && object.material) {
+      object.material.color.set(color);
+    }
+  };
+}
+//body
+setColor("goldCar", () => carBody, 0xb8860b);
+setColor("blueCar", () => carBody, 0x0000ff);
+setColor("redCar", () => carBody, 0xff0000);
+//rim
+setColor("blackRim", () => carRim, 0x000000);
+setColor("silverRim", () => carRim, 0xc0c0c0);
+setColor("greenRim", () => carRim, 0x00ff00);
 
 function animate() {
   requestAnimationFrame(animate);
